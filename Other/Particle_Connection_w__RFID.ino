c9be377e9562e2edb33ee1759a9998f2efa295a7
Ali Toyserkani <ali.toyserkani1998@gmail.com>
2017-01-29 10:02:13 -0500

    added photon connection code

---
+++ Other/Particle_Connection_w__RFID.ino
@@ -0,0 +1,25 @@
+
+typedef int Data;
+unqualified id;
+
+void setup() {
+  pinMode(13, OUTPUT); //Set the LED pin as an output
+  pinMode(0,INPUT); //RX pin arduino input
+  pinMode(1,OUTPUT); //TX pin arduino output
+  Serial.begin(9600); //Start the UART Serial comm.
+  delay(2000);
+}
+void loop() {
+  while(Serial.available()) // if anything is in the Serial Buffer, run this code
+  {
+  Data = Serial.read(); // Read whats in the Serial buffer and store to DATA
+  }
+  if(Data == 0) // If the Data  is 0, turn off the LED
+  {
+    digitalWrite(13,LOW);
+  }
+  else if(Data == 1) // If the Data is 1 turn on the LED
+  {
+    digitalWrite(13,HIGH);
+  }
+}
